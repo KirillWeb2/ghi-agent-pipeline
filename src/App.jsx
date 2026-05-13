716d6a9fada573ced9229aa37010f117db3e8290
agent-runner <agent-runner@local>
2026-05-13 07:05:03 +0000

    agent (rework): [agent] (#69)

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import PostsList from './pages/PostsList';
-import CreatePost from './pages/CreatePost';
-import Profile from './pages/Profile';
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import PostsList from './pages/PostsList'
+import CreatePost from './pages/CreatePost'
+import Profile from './pages/Profile'
+import './App.css'
 
 function App() {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<PostsList />} />
-          <Route path="/create" element={<CreatePost />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </Layout>
-    </Router>
-  );
+    <Layout>
+      <Routes>
+        <Route path="/" element={<PostsList />} />
+        <Route path="/create" element={<CreatePost />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Layout>
+  )
 }
 
-export default App;
+export default App
